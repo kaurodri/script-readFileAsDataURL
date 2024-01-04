05e10c08fe71b20ecf430d536f88c19979dd81bb
HieA1lpha <engkauanrodrigues@gmail.com>
2024-01-04 00:20:05 -0300

    2024-01-04

---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+function base(filePath) {
+    try {
+        const data = fs.readFileSync(filePath);
+        const base64Data = data.toString('base64');
+        const mimeType = 'application/octet-stream'; // defina o tipo MIME apropriado
+        const dataURL = `data:${mimeType};base64,${base64Data}`;
+        return dataURL;
+    } catch (error) {
+        console.error("Erro ao ler o arquivo:", error);
+        return null;
+    }
+}
+
+function readFileAsDataURL(file) {
+    const filePath = path.join(__dirname, file);
+    const dataURL = base(filePath);
+    return dataURL;
+}
+
+module.exports = readFileAsDataURL;
